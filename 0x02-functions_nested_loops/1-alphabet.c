1a6de4fe41b7c956b9e9f960aad8e6cc386c12eb
“Lorraine <lorrainemthethwa16@gmail.com>
2023-02-21 16:04:23 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - Print the lowercase alphabet, followed by a new line
+ * print_alphabet - Print the lowercase alphabet followed by a new line
  */
 
 void print_alphabet(void)
