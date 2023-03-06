0a774713722d5bf0c35eb03a4036ef4c56d2f5d9
“Lorraine <lorrainemthethwa16@gmail.com>
2023-03-06 14:08:10 -0800

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,15 @@
+#include "main.h"
+
 /**
  * _strchr - locates a character in a string
- * @s: pointer to the string to search
- * @c: the character to locate
  *
- * Return: pointer to the first occurrence of c in s, or NULL if c is not found
+ * @c: Character to search
+ *
+ * @s: Pointer to the string to search
+ *
+ * Return: a pointer to the first occurrence of the character
  */
+
 char *_strchr(char *s, char c)
 {
 	while (*s)
