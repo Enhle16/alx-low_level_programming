36e2eddecd6eecc240ac02aa8e692c17859cd38e
“Lorraine <lorrainemthethwa16@gmail.com>
2023-03-08 12:24:12 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,19 +1,18 @@
-include "main.h"
-
+#include "main.h"
 /**
- * _strlen_recursion - returns the length of the string
- * @s: string
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _strlen_recursion - length of string
+ * @s:string
+ * Return:int
  */
-
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		return (1 + _strlen_recursion(s + 1));
-	}
-	return (0);
-}
+	int i = 0;
 
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+
+	}
+	return (i);
+}
